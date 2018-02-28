29de7a883e663519abd5a0238ef5261bcbd1fffa
logonrm <logonrm@fiap.com.br>
2018-02-28 11:45:44 -0300

    quartil definitions

--- laoding_positions.R
+++ laoding_positions.R
@@ -1,15 +1,30 @@
 dados_paises <- read_csv2("data/dados_1997_2011_paises_csv.csv", locale = locale(encoding = 'latin1'))
 
 # min, max and mean by idh
-# summarise create a table with the values
+# summarise aggregate values
 dados_paises %>% 
   summarise(
     idh_min = min(idh),
     idh_max = max(idh),
     idh_medium = mean(idh),
-    idh_moda = mlv(idh, method = 'mfv')$M
+    idh_moda = mlv(idh, method = 'mfv')$M,
+    idh_0.10 = quantile(idh, probs = 0.1), # 10% da amostra é menor ou igual que o valor
+    idh_0.90 = quantile(idh, probs = 0.90), # 90% da amostra é menor ou igual que o valor
+    idh_q1 = quantile(idh, probs = 0.25), #25% da amostra é menor que o valor
+    idh_q2 = quantile(idh, probs = 0.5), # 50% da amostra é menor que o valor
+    idh_q3 = quantile(idh, probs = 0.75) # 75% da amostra é menor que o valor
   )
 
 dados_paises %>% 
   filter(idh == min(idh))
 
+
+#Calculate quartils from IDH to dataset
+dados_paises %>%
+  mutate(idh_quartil = 
+           cut (
+             idh, quantile(idh, c(0,0.25,0.5,0.75,1)),
+             include.lowest = TRUE,
+             labels = c('D', 'C', 'B', 'A')
+             )
+         )
